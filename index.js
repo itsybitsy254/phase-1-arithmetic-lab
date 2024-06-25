ef6405286d6af29d8b7f439556edd3f383bf1217
Vitalis Ian <viterlis254@gmail.com>
2024-06-25 07:46:53 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-
 // Write your code here
+let num1= 2;
+
+let num2= 31;
+
+const multiply= num1*num2;
+
+console.log(multiply)
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+ 
+
+let num3 = 54;
+let num4 = 5;
+
+const mod = num3 % num4;
+console.log(mod)
+
+let max = Math.max(1,5,10,15,20);
+    
+ console.log(max)
